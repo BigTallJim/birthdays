d5e37ba1dccaf7b8655cf6c3228759a27f2394bc
James Holton <james_holton@yahoo.co.uk>
2019-08-26 21:59:01 +0100

    First and last commit

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,20 @@
+class Birthday
+  def initialize
+    @birthday_hash = {}
+  end
+  def saveFriend(name, birthday)
+    @birthday_hash[name] = birthday
+  end
+  def displayFriends()
+    @birthday_hash.each do |key, value|
+      puts "Name #{key} Birthday #{value}"
+    end
+  end
 
+  def birthdays_today()
+    today = Time.now.strftime("%d/%m")
+    @birthday_hash.each do |key, value|
+      return "It's #{key}'s Birthday today" if today == value[0..4]
+    end
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,14 @@
-
+require "birthday_list.rb"
+describe Birthday do
+  it "add name and birthday to hash map" do
+    expect(subject.saveFriend("James", "23/11/1974")).to eq("23/11/1974")
+  end
+  it "displays birthdays if they are today" do
+    allow(Time).to receive(:now).and_return Time.new(2019,8,26)
+    subject.saveFriend("Person1", "24/08/1974")
+    subject.saveFriend("Person2", "25/08/1974")
+    subject.saveFriend("James", "26/08/1974")
+    subject.saveFriend("Person3", "27/08/1974")
+    expect(subject.birthdays_today).to eq("It's James's Birthday today")
+  end
+end
